6c3fe3a1e0ccdf835d70c1f01e7cb0a19cfd16cc
clarelee-pvcc <158596996+clarelee-pvcc@users.noreply.github.com>
2024-03-26 23:39:54 -0400

    Add files via upload

---
+++ tuition2.py
@@ -0,0 +1,79 @@
+# Name: Clare Lee
+# Prog Purpose: This program ocmputes PVCC college tuition & fees based on number of credits
+# PVCC Fee Rates are from # https://www.pvcc.edu/tuition-and-fees
+
+import datetime
+# define tuition & fee rates
+RATE_TUITION_IN = 146.26
+RATE_TUITION_OUT = 336.21
+RATE_CAPITAL_FEE = 23.5
+RATE_INSTITUTION_FEE= 1.75
+RATE_ACTIVITY_FEE = 2.9
+
+#define global variables
+inout = 1 # 1 means in-state, 2 means out-of-state
+numcredits = 0
+scholarship_amt = 0
+
+tuition_amt = 0
+inst_fee = 0
+cap_fee = 0
+act_fee = 0
+total = 0
+balance = 0
+
+################ define program functions ##################
+def main():
+    
+    more = True
+    while more:
+        get_user_data()
+        perform_calculations()
+        display_results()
+
+        yesno =input("\nWould you like to calculate tuition & fees for another student? (Y/N): ")
+        if yesno == "n" or yesno == "N":
+            more = False
+            print("Thank you for enrollng at PVCC. Enjoy the semester!")
+
+def get_user_data():
+    global inout, numcredits, scholarshipamt
+    print('********PIEDMONT VIRGINIA COMMUNITY COLLEGE Tuition & Fees *********')
+    inout = int(input("Enter a 1 for IN-STATE ; enter a 2 for OUT-OF-STATE: "))
+    numcredits = int(input("Number of credits registered  for: "))
+    scholarship_amt = int(input("Scholarship amount received: "))
+
+def perform_calculations():
+        global tuition, inst_fee, cap_fee, act_fee, total, balance
+
+        if inout == 1:
+            tuition = numcredits * RATE_TUITION_IN
+            cap_fee = 0
+        else:
+            tuition = numcredits * RATE_TUITION_OUT
+            cap_fee = numcredits * RATE_CAPITAL_FEE
+
+        inst_fee = RATE_INSTITUTION_FEE
+    # calculations for the other fees, total, and balance go here
+
+def display_results():
+    currency = '8,.2f'
+    line = '-------------------------------------------------'
+
+    print('----------------------------------')
+    print('******** PIEDMONT VIRGINIA COMMUNITY COLLEGE ********')
+    print('--------------------------------')
+    print('------  Tuition and Fees Report -----')
+    print(line)
+    print(line)
+    print('Tuition                 $ ' + format(tuition,currency))
+    print('Capital Fee             $ ' + format(cap_fee,currency))
+    print('---------------------')
+    print(str(datetime.datetime.now()))
+
+#print statements for the other fees, total, and balance go here.
+
+########### call on main program to execute ##########
+main()
+
+    
